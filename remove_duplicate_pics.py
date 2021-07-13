4f63d022e58ea1ded54f198db35da4b0131bfce3
Jan Belterman <jan@enjoj.nl>
2021-07-13 15:25:01 +0200

    Clean code update

--- remove_duplicate_pics.py
+++ remove_duplicate_pics.py
@@ -1,13 +1,21 @@
 import os
 
-files = os.listdir("")
+DIR = ''
+
+def removeFile(filename):
+    try:
+        os.remove(DIR + filename)
+    except:
+        print('Failed to remove: ' + filename)
+        return
+    print('Removed: ' + filename)
+
+
+allFiles = os.listdir(DIR)
+files = [s for s in allFiles if s.endswith('.MOV')]
+jpgFiles = [s for s in allFiles if s.endswith('.JPG')]
 
 for filename in files:
-    if (filename.endswith('.MOV')):
-        matching = [s for s in files if s.startswith(filename[:-4]) and s.endswith('.JPG')]
-        if (len(matching) > 0):
-            try:
-                os.remove("" + filename)
-            except:
-                print('Failed to remove: ' + filename)
-            print('Removed: ' + filename)
+    matching = [s for s in jpgFiles if s.startswith(filename[:-4])]
+    if (len(matching) > 0):
+        removeFile(filename)
